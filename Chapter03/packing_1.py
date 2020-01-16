3e89cf14c28e91c01dfd3dbe412075e3be291c4e
aisolab <bsk0130@gmail.com>
2020-01-16 10:50:48 +0900

    Chapter03

---
+++ Chapter03/packing_1.py
@@ -0,0 +1,28 @@
+"""
+packing / unpacking
+"""
+
+USERS = [(i, f"first_name_{i}", f"last_name_{i}") for i in range(1, 1000)]
+
+
+class User:
+    def __init__(self, user_id, first_name, last_name):
+        self.user_id = user_id
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.user_id!r}, {self.first_name!r}, {self.last_name!r})"
+
+
+def bad_users_from_rows(dbrows) -> list:
+    """A bad case (non-pythonic) of creating ``User``s from DB rows."""
+    return [User(row[0], row[1], row[2]) for row in dbrows]
+
+
+def users_from_rows(dbrows) -> list:
+    """Create ``User``s from DB rows."""
+    return [
+        User(user_id, first_name, last_name)
+        for (user_id, first_name, last_name) in dbrows
+    ]
